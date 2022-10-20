cab097e09d25112967f23c6aebede435baead3b2
leonardoih <leonardoih090217@gamil.com>
2022-10-19 21:45:03 -0500

    numeros pares e impares

---
+++ ejercicio8.js
@@ -0,0 +1,9 @@
+function ejecutar() {
+	const numberB = parseInt(prompt('join a number'));
+
+	if (numberB % 2 == 0) {
+		alert('the number is even');
+	} else {
+		alert('the number is odd');
+	}
+}
